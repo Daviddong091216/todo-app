090c73f762b0c9e947c5cea63aa3fa665084005d
David Dong <daviddong201612@gmail.com>
2021-02-26 11:21:24 -0600

    2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Vue 3 Todo App</h1>
+  <form @submit.prevent="addNewTodo">
+    <label>New Todo</label>
+    <input v-model="newTodo" />
+    <button>Add New Todo</button>
+  </form>
+  <h2>{{ newTodo }}</h2>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id" class="cursor">
+      <h3 :class="{underLine: todo.done}" @click="toggleDone(todo)">{{ todo.content }}</h3>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from "vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const newTodo = ref("");
+    const todos = ref([]);
+
+    function addNewTodo() {
+      console.log(newTodo.value);
+      todos.value.push({
+        id: 1,
+        done: false,
+        content: newTodo.value,
+      });
+      newTodo.value = "";
+    }
+    function toggleDone(todo){
+      todo.done =!todo.done;
+    }
+    return {
+      toggleDone,
+      todos,
+      newTodo,
+      addNewTodo,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: sans-serif;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  font-size: 2em;
+  width: 80%;
+  margin: 0 auto;
+}
+.cursor{
+  cursor: pointer;
+}
+.underLine{
+  text-decoration: line-through;
 }
 </style>
